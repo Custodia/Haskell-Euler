92f86e119d7502ac3c36c5e5a17a405723b8478b
Jani Makela <jani.makela@pp3.inet.fi>
2015-10-30 20:12:49 +0200

    Solved problem 8

--- euler7better.hs
+++ euler7better.hs
@@ -3,11 +3,13 @@ isPrime a = isPrimeHelper a primes
 
 isPrimeHelper :: Int -> [Int] -> Bool
 isPrimeHelper a (p:ps)
+	| m - 1 /= 0 && m + 1 /= 6 = False
 	| p*p > a = True
 	| a `mod` p == 0 = False
 	| otherwise = isPrimeHelper a ps
+	where m = a `mod` 6
 
-primes = 2 : filter isPrime [3..]
+primes = 2 : 3 : filter isPrime [5..]
 
 solution :: Int -> Int
-solution n = primes !! n
+solution n = primes !! (n - 1)
